3613e1bc2733db7e0bab65d63ec856fbe6b23cea
jlogosselin <jlgosselingithub@gmail.com>
2022-08-15 11:58:56 +0100

    new method added for input verification

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -5,63 +5,65 @@ import java.util.Scanner;
 public class RockPaperScissors {
 
     public static void main(String[] args) {
-
-        Scanner userInput = new Scanner(System.in);
         String playAgain = "";
-
         do{
             int playerWins = 0;
             int computerWins = 0;
             int draws = 0;
-
-            int numberOfRounds = chooseNumberRounds();
-
+            int numberOfRounds = enterInteger(1);
             introMessage();
 
             for(int i=0; i<numberOfRounds; i++) {
 
-                System.out.println("Play (1) for Rock, (2) for Paper, (3) for Scissors");
-
-                //player's move:
                 int playerMove = playerMove();
-
-                //computer's turn, generating random value between 1 and 3:
                 int computerMove = computerMove();
-
                 System.out.println("You played " + showPlay(playerMove) + ", computer played " + showPlay(computerMove));
-
                 int[] scoreContainer = {playerMove, computerMove, draws, computerWins, playerWins};
-                int[] setScores = settingScores(scoreContainer, i);
-
-                draws = setScores[2];
-                computerWins = setScores[3];
-                playerWins = setScores[4];
+                settingScores(scoreContainer, i);
+                draws = scoreContainer[2];
+                computerWins = scoreContainer[3];
+                playerWins = scoreContainer[4];
             }
 
-            //displaying final results:
             displayFinalResults(draws, computerWins, playerWins);
-
-            //displaying the winner:
             displayWinner(computerWins, playerWins);
-
             playAgainMessage();
             playAgain = playAgain();
 
         }while(playAgain.equals("Yes"));
-
         finalMessage();
-
     }
 
-    public static int chooseNumberRounds(){
+    public static int enterInteger(int x){
         Scanner userInput = new Scanner(System.in);
+        String n;
+        boolean r1, r2=false;
+        do{
+            if(x == 1) enterNumberRoundsMessage();
+            else if (x == 2) enterPlayerMove();
+            n = userInput.nextLine();
+            r1 = isNumeric(n);
+            if(r1){
+                if(Integer.parseInt(n) >= 1 && Integer.parseInt(n) <= 10){
+                    r1 = true;
+                    r2 = true;
+                }
+            }
+        }while(r1 == false || r2 == false);
+        return Integer.parseInt(n);
+    }
+
+    public static void enterNumberRoundsMessage(){
         System.out.println("Choose a number of rounds, between 1 and 10: ");
-        int numberOfRounds = Integer.parseInt(userInput.nextLine());
-        while(numberOfRounds < 1 || numberOfRounds > 10){
-            System.out.println("Sorry, incorrect input. Choose a number of rounds, between 1 and 10: ");
-            numberOfRounds = Integer.parseInt(userInput.nextLine());
-        }
-        return numberOfRounds;
+    }
+
+    public static void enterPlayerMove(){
+        System.out.println("Play (1) for Rock, (2) for Paper, (3) for Scissors");
+    }
+
+    public static boolean isNumeric(String s) {
+        boolean result = s.matches("-?\\d+");
+        return result;
     }
 
     public static void introMessage(){
@@ -69,62 +71,30 @@ public class RockPaperScissors {
     }
 
     public static int playerMove(){
-        Scanner userInput = new Scanner(System.in);
-        int playerMove = Integer.parseInt(userInput.nextLine());
-        while(playerMove < 1 || playerMove > 3){
-            System.out.println("Sorry, incorrect input! Please play (1) for Rock, (2) for Paper, (3) for Scissors");
-            playerMove = Integer.parseInt(userInput.nextLine());
-        }
-        return playerMove;
+        return enterInteger(2);
     }
 
     public static int computerMove(){
-        Scanner userInput = new Scanner(System.in);
-        int min = 1;
-        int max = 3;
+        int min = 1, max = 3;
         int computerMove = (int)(Math.random()*(max-min+1)+min);
         return computerMove;
     }
 
-    public static int[] settingScores(int[] scoreContainer, int i) {
-
+    public static void settingScores(int[] scoreContainer, int i) {
         if (scoreContainer[0] == 1) {
-
-            if (scoreContainer[1] == 1) {
-                scoreContainer[2] += roundIsDraw(i);
-            }
-            else if (scoreContainer[1] == 2) {
-                scoreContainer[3] += computerWinsRound(i);
-            }
-            else if (scoreContainer[1] == 3) {
-                scoreContainer[4] += playerWinsRound(i);
-            }
+            if (scoreContainer[1] == 1) scoreContainer[2] += roundIsDraw(i);
+            else if (scoreContainer[1] == 2) scoreContainer[3] += computerWinsRound(i);
+            else if (scoreContainer[1] == 3) scoreContainer[4] += playerWinsRound(i);
         }
         else if (scoreContainer[0] == 2) {
-
-            if (scoreContainer[1] == 1) {
-                scoreContainer[4] += playerWinsRound(i);
-            }
-            else if (scoreContainer[1] == 2) {
-                scoreContainer[2] += roundIsDraw(i);
-            }
-            else if (scoreContainer[1] == 3) {
-                scoreContainer[3] += computerWinsRound(i);
-            }
+            if (scoreContainer[1] == 1) scoreContainer[4] += playerWinsRound(i);
+            else if (scoreContainer[1] == 2) scoreContainer[2] += roundIsDraw(i);
+            else if (scoreContainer[1] == 3) scoreContainer[3] += computerWinsRound(i);
         }
-        else if (scoreContainer[0] == 3) {
-
-            if (scoreContainer[1] == 1) {
-                scoreContainer[3] += computerWinsRound(i);
-            }
-            else if (scoreContainer[1] == 2) {
-                scoreContainer[4] += playerWinsRound(i);
-            }
-            else if (scoreContainer[1] == 3) {
-                scoreContainer[2] += roundIsDraw(i);
-            }
-        }
-        return scoreContainer;
+        else if (scoreContainer[0] == 3)
+            if (scoreContainer[1] == 1) scoreContainer[3] += computerWinsRound(i);
+            else if (scoreContainer[1] == 2) scoreContainer[4] += playerWinsRound(i);
+            else if (scoreContainer[1] == 3) scoreContainer[2] += roundIsDraw(i);
     }
 
     public static void displayFinalResults(int draws, int computerWins, int playerWins){
@@ -134,15 +104,9 @@ public class RockPaperScissors {
     }
 
     public static void displayWinner(int computerWins, int playerWins){
-        if (computerWins == playerWins) {
-            System.out.println("It's a draw!");
-        }
-        else if (computerWins < playerWins) {
-            System.out.println("YOU WIN!");
-        }
-        else {
-            System.out.println("Computer wins game...");
-        }
+        if (computerWins == playerWins) System.out.println("It's a draw!");
+        else if (computerWins < playerWins) System.out.println("YOU WIN!");
+        else System.out.println("Computer wins game...");
     }
 
     public static int computerWinsRound(int round){
@@ -161,14 +125,9 @@ public class RockPaperScissors {
     }
 
     public static String showPlay(int n){
-        if(n == 1){
-            return "Rock";
-        }
-        else if(n == 2){
-            return "Paper";
-        }
-        else
-            return "Scissors";
+        if(n == 1) return "Rock";
+        else if(n == 2) return "Paper";
+        else return "Scissors";
     }
 
     public static void playAgainMessage(){
